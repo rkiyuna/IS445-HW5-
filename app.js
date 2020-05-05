6b06ac2c35518a73dba83b8d6e0682294511f749
Reece <rkeeyuna@gmail.com>
2020-05-04 17:05:32 -0700

    pseudo

--- app.js
+++ app.js
@@ -2,13 +2,12 @@ function primeNum() {
   var num1 = parseInt(document.getElementById("num1").value);
   var num2 = parseInt(document.getElementById("num2").value);
 
-  // check if the numbers fit criteria
+  // evaluate numbers
   if (num1 <= 2 || num1 >= 100 || num2 <= 2 || num2 >= 100) {
     document.getElementById("msg").innerHTML =
       "Invalid input, please try again.";
   } else if (num1 >= 2 && num1 <= 100 && num2 >= 2 && num2 <= 100) {
     calculatePrimeNum(num1, num2);
-    //document.getElementById("msg").innerHTML = "";
   }
 }
 function isPrime(num) {
@@ -21,8 +20,8 @@ function isPrime(num) {
   }
   return res;
 }
+//calculation
 function calculatePrimeNum(firstNum, secondNum) {
-  //alert("calculating");
   var pNumArray = [];
   var sNum = 0;
   var bNum = 0;
